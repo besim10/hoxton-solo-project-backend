a555d2c46113e40b70ed923c8170f7515ca02fcd
Besim <besimsokoli10@gmail.com>
2022-04-01 12:05:19 +0200

    added endpoint for login

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 import express from 'express'
 import cors from 'cors'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
@@ -13,7 +15,33 @@ app.get('/hospital', async(req,res) =>{
     const hospital = await prisma.hospital.findUnique({where: {name: 'Medica+'}})
     res.send(hospital)
 })
+ function createToken(id: number){
+    //@ts-ignore
+    const token = jwt.sign({id: id}, process.env.SECRET_KEY, {
+        expiresIn: '3days'
+    })
+    return token
+}
+app.post('/login', async (req,res) => {
+    const {email, password} = req.body
 
+    try{
+        const admin = await prisma.admin.findUnique({where: {email: email}})
+        //@ts-ignore
+        const passwordMatches = bcrypt.compareSync(password, admin.password)
+
+        if(admin && passwordMatches){
+            res.send({admin, token: createToken(admin.id)})
+        }
+        else{
+            throw Error('Email or Password Invalid!')
+        }
+    }catch(err){
+        //@ts-ignore
+        res.status(400).send({error: err.message})
+    }
+
+})
 app.listen(PORT, () =>{
     console.log(`Server up and running on http://localhost:${PORT}`)
 })
